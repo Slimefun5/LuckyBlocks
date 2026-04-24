ff2bb73ef599a42e6b3929c3ac8a305da2b418c9
Intisy <finn@birich.de>
2026-04-24 21:37:48 +0200

    Add github-gradle plugin for dependency management and release publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,20 @@
 plugins {
     java
     id("com.gradleup.shadow") version "9.3.2"
+    id("io.github.intisy.github-gradle") version "1.3.8"
 }
 
 group = "io.github.thebusybiscuit"
 version = "UNOFFICIAL"
 description = "SlimefunLuckyBlocks is a Slimefun addon that adds Lucky Blocks."
+
+github {
+    accessToken = System.getenv("GITHUB_TOKEN") ?: ""
+}
+
+publishGithub {
+    tag = System.getenv("GITHUB_REF_NAME")
+}
 
 java {
     toolchain {
@@ -21,7 +30,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly(fileTree("${rootDir}/../Slimefun5/build/libs") { include("*.jar") })
+    githubCompileOnly("Slimefun5:Slimefun5:v5.0.3")
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
     implementation("org.bstats:bstats-bukkit:3.0.2")

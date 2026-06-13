ba13b860cd15cfc5e2567dc0a28972440580cdf5
intisy (Finn Birich) <finn@birich.de>
2026-06-14 01:04:15 +0200

    build(ci): consume core via github-gradle (Slimefun5:Slimefun5:v5.2.1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ repositories {
 
 dependencies {
     implementation("com.github.Slimefun5:SlimefunMetrics:master-SNAPSHOT")
-    compileOnly(files("../../core/Slimefun5/core/build/libs/Slimefun v5.0.0-UNOFFICIAL-MC26.1.2.jar"))
+    githubCompileOnly("Slimefun5:Slimefun5:v5.2.1")
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
     
@@ -67,6 +67,7 @@ tasks {
     }
 
     shadowJar {
+        relocate("org.bstats", "luckyblocks.libs.bstats")
         archiveFileName.set("SlimefunLuckyBlocks v${project.version}.jar")
                 exclude("META-INF/**")
     }

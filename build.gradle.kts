619afc71b342d61b734cfd68433f8f210664710d
intisy (Finn Birich) <finn@birich.de>
2026-07-08 00:08:06 +0200

    build: move gradle conventions to Slimefun5/gradle repo; slim addon build

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,37 +7,16 @@ plugins {
 group = "io.github.thebusybiscuit"
 description = "SlimefunLuckyBlocks is a Slimefun addon that adds Lucky Blocks."
 
-// Shared Slimefun-addon build conventions (Java 8, spigot-api baseline, core dep, publish, shadow, version).
-apply(from = "https://raw.githubusercontent.com/Slimefun5/workflows/stable/slimefun-addon.gradle")
-
-repositories {
-    maven("https://jitpack.io")
-}
+apply(from = "https://raw.githubusercontent.com/Slimefun5/gradle/stable/slimefun-addon.gradle")
 
 dependencies {
     githubImplementation("Slimefun5:SlimefunMetrics:v1.0.0")
     implementation("org.bstats:bstats-bukkit:2.2.1")
-
-    testImplementation(platform("org.junit:junit-bom:5.11.4"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.mockito:mockito-core:5.15.2")
-    testImplementation("org.slf4j:slf4j-simple:2.0.16")
-    testImplementation("org.mockbukkit.mockbukkit:mockbukkit-v1.21:4.107.0") {
-        exclude(group = "org.jetbrains", module = "annotations")
-    }
-}
-
-configurations {
-    testImplementation {
-        extendsFrom(configurations.compileOnly.get())
-    }
 }
 
 tasks {
     shadowJar {
         relocate("org.bstats", "luckyblocks.libs.bstats")
+        archiveFileName.set("LuckyBlocks-${project.extra["displayVersion"]}.jar")
     }
-    compileTestJava { enabled = false }
-    test { enabled = false }
 }
